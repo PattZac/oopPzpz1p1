5cac7e766f266ff4a6b68b92b8d8f8758ad4a8ed
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 17:04:59 +0100

    Add files via upload

---
+++ oopPzpz1p1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopPzpz1p1
+{
+    class Fruit
+    {
+        string vrsta, boja;
+        bool jeLiSlatko;
+        public Fruit(string v)
+        {
+            this.vrsta = v;
+        }
+        public Fruit(string v,string b, bool jLS)
+        {
+            this.vrsta = v;
+            this.boja = b;
+            this.jeLiSlatko = jLS;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit x = new Fruit("Jagoda", "Crvena", true);
+            Fruit y = new Fruit("Limun", "Žuta", false);
+            Fruit z = new Fruit("Avokado");
+        }
+    }
+}
